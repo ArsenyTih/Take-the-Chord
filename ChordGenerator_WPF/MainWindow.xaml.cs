2be857e863d73f5d27ff24c59064bab5a19ad11b
Влад is love <97906766+Art-Rock-Climber@users.noreply.github.com>
2024-08-01 22:24:05 +0500

    Update MainWindow.xaml.cs
    Подправил комментарий

--- ChordGenerator_WPF/MainWindow.xaml.cs
+++ ChordGenerator_WPF/MainWindow.xaml.cs
@@ -4,7 +4,7 @@ using System.Windows.Input;
 namespace ChordGenerator_WPF
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
